0e0a195ef82615bdcf20ec4a4ec57a7231a0ce28
cookie <cookie@cookie-MacPro.local>
2020-03-30 10:48:41 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+"""
+print('Interst Calculator:')
+amount = float(input('Principal amount?'))
+roi = float(input('Rate of Interest ?'))
+years = int(input('Duration (no. of years) ?'))
+total = (amount * pow(1 + (roi/100), years))
+interest = total - amount
+print('\nInterest = %0.2f' %interest)
+"""
